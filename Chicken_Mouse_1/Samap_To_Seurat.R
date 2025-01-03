de536ecc92405a1a2b3610254e0957e9646d495f
twig28 <155920332+twig28@users.noreply.github.com>
2025-01-02 20:20:33 -0500

    adding metadata of cell type to object

--- Chicken_Mouse_1/Samap_To_Seurat.R
+++ Chicken_Mouse_1/Samap_To_Seurat.R
@@ -1,8 +1,9 @@
 #File to convert samap outputs into different formats in R for further analysis
 install = TRUE
-annDataPickleFileName = "samap_adata.pkl"
+annDataPickleFileName = "Samap_adata.pkl"
+annDataRawPickleFileName = "samap_raw_adata.pkl"
 samapObjectPickleFilename = "samap_object.pkl"
-h5adFileName = "Samap_adata"
+h5adFileName = "samap_adata"
 
 if(install){
   remove.packages('reticulate')
@@ -12,7 +13,6 @@ if(install){
   devtools::install_github(repo = "mojaveazure/loomR", ref = "develop")
   devtools::install_github("cellgeni/sceasy")
   library("Seurat")
-  library(loomR)
   require("reticulate")
   reticulate::py_install("samap")
   reticulate::py_install("sam-algorithm")
@@ -22,7 +22,6 @@ if(install){
   reticulate::py_install("ipywidgets")
   reticulate::py_install("ipyevents")
   reticulate::py_install("numpy==1.23.5")
-  reticulate::py_install("loompy")
 }
 
 #This Will Use a Lot of memory as it loads the entirety of the samap implementation/object
@@ -30,17 +29,55 @@ samap_pickle_data <- reticulate::py_load_object(paste0("./", samapObjectPickleFi
 
 anndata_pickle_data <- reticulate::py_load_object(paste0("./", annDataPickleFileName))
 
-#Load into seurat using h5ad
+anndata_raw_pickle_data <- reticulate::py_load_object(paste0("./", annDataRawPickleFileName))
+
+#Load samap anndata into seurat using h5ad
 sceasy::convertFormat(paste0(h5adFileName, ".h5ad"), from="anndata", to="seurat",
                       outFile=paste0(h5adFileName, ".rds"))
 seuratObjecth5ad = readRDS(paste0(h5adFileName, ".rds"))
 
+#Print Gene Names
 gene_List <- rownames(seuratObjecth5ad@assays$RNA@counts)
 print(gene_List)
 
-print(Idents(seuratObjecth5ad))
 
-#Load into seurat using h5ad
-#sceasy::convertFormat("chMG.h5ad", from="anndata", to="seurat",
-#                      outFile=paste0("testchMG", ".rds"))
-#seuratObjecth5adTEST = readRDS(paste0(h5adFileName, ".rds"))
+
+
+#---For Sparse Data---
+
+
+
+
+
+
+#Convert Sparse R Object into h5ad for Samap Sparse Run
+h5adFileName <- "samap_adata_sparse"
+sparse_RDS <- readRDS("chMG_NMsubset.rds")
+
+#add cell type label and subset out FI and NFI
+print(sparse_RDS@active.ident)
+sparse_RDS <- AddMetaData(
+  object = sparse_RDS,
+  metadata = Idents(sparse_RDS),
+  col.name = "active_ident"
+)
+print(unique(sparse_RDS@meta.data$treatment))
+
+seurat_obj_subset <- subset(
+  sparse_RDS,
+  subset = treatment != "FI" & treatment != "NFI"
+)
+
+table(seurat_obj_subset@meta.data$treatment)
+
+#Save for Samap Run
+sceasy::convertFormat(seurat_obj_subset, from="seurat", to="anndata",
+                      outFile=paste0(h5adFileName, ".h5ad"))
+
+#---SAMAP RUN----
+
+#Load Samap Run
+h5adFileName = "samap_h5ad_sparse"
+sceasy::convertFormat(paste0(h5adFileName, ".h5ad"), from="anndata", to="seurat",
+                      outFile=paste0(h5adFileName, ".rds"))
+seuratObjectSparse = readRDS(paste0(h5adFileName, ".rds"))
